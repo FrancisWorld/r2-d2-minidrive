d875b8e540375c071bba891f36611596aaa68023
Chico <francisco.olsilva.0307@gmail.com>
2025-02-23 15:22:15 -0300

    initialize project structure with Spring Boot and Maven setup

---
+++ src/main/java/com/katydevs/r2_d2_minidrive/R2D2MinidriveApplication.java
@@ -0,0 +1,13 @@
+package com.katydevs.r2_d2_minidrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2D2MinidriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R2D2MinidriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/katydevs/r2_d2_minidrive/R2D2MinidriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katydevs.r2_d2_minidrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R2D2MinidriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
